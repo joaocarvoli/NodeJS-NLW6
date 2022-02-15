1313352496de604cfbf12b7dc88f784eca427f0f
Joao Victor <joaocarvoli@hotmail.com>
2022-02-15 14:26:19 -0300

    dealing with errors in server layer

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,14 +1,28 @@
-import "reflect-metadata"
-import express, { response } from "express"
+import "reflect-metadata";
+import express, { NextFunction, Response, Request } from "express";
+import "express-async-errors";
 import { router } from "./routes"
 require('dotenv').config({ path: 'src'+'/.env' });
 
-import "./database"
+import "./database";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(router)
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
